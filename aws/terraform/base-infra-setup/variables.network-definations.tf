9464d6647491751f67066037b9ab3f8e18b22dfa
Aakash Kumar <aakash.kumar1980@yahoo.com>
2023-12-31 17:19:37 -0700

    updated

--- aws/terraform/base-infra-setup/variables.network-definations.tf
+++ aws/terraform/base-infra-setup/variables.network-definations.tf
@@ -1,4 +1,9 @@
 /**
+  1. Subnet Calculator: https://www.davidc.net/sites/default/subnets/subnets.html
+  2. Concept: https://www.howtoinmagento.com/2019/05/simply-calculate-aws-subnets-cidr.html
+  3. Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+  4. Online Terraform Console: https://terraform-online-console.com/
+
   Defining the network configuration for the region. e.g.:
   region_nvirginia (map)
   └─ vpc (map)
@@ -37,12 +42,13 @@ variable "region_configurations" {
         vpc_a = {
           cidr_block = "10.0.0.0/24"
           subnets = {
-            "1" = { type = "generic", cidr_block = "10.0.0.0/28", availability_zone_index = "a" }
-            "2" = { type = "public",  cidr_block = "10.0.0.16/28", availability_zone_index = "a" }
-            "3" = { type = "public",  cidr_block = "10.0.0.32/27", availability_zone_index = "b" }
-            "4" = { type = "private", cidr_block = "10.0.0.64/26", availability_zone_index = "a" }
-            "5" = { type = "private", cidr_block = "10.0.0.128/26", availability_zone_index = "b" }
-            "6" = { type = "private", cidr_block = "10.0.0.192/26", availability_zone_index = "c" }
+            "1" = { type = "generic", cidr_block = "10.0.0.0/27", availability_zone_index = "a" }
+            "2" = { type = "public",  cidr_block = "10.0.0.32/27", availability_zone_index = "a" }
+            "3" = { type = "public",  cidr_block = "10.0.0.64/27", availability_zone_index = "b" }
+            "4" = { type = "private", cidr_block = "10.0.0.96/27", availability_zone_index = "a" }
+            "5" = { type = "private", cidr_block = "10.0.0.128/27", availability_zone_index = "b" }
+            "6" = { type = "private", cidr_block = "10.0.0.160/27", availability_zone_index = "c" }
+            # free CIDRs: 10.0.0.192/27, 10.0.0.224/27
           }
         }
         vpc_b = {
@@ -50,7 +56,8 @@ variable "region_configurations" {
           subnets = {
             "1" = { type = "generic", cidr_block = "172.16.0.0/28", availability_zone_index = "a" }
             "2" = { type = "public",  cidr_block = "172.16.0.16/28", availability_zone_index = "b" }
-            "3" = { type = "private", cidr_block = "172.16.0.32/27", availability_zone_index = "c" }
+            "3" = { type = "private", cidr_block = "172.16.0.32/28", availability_zone_index = "c" }
+            # free CIDRs: 172.16.0.48/28
           }
         }
       }
@@ -63,15 +70,17 @@ variable "region_configurations" {
           subnets = {
             "1" = { type = "generic", cidr_block = "192.168.0.0/28", availability_zone_index = "a" }
             "2" = { type = "public",  cidr_block = "192.168.0.16/28", availability_zone_index = "b" }
-            "3" = { type = "private", cidr_block = "192.168.0.32/27", availability_zone_index = "c" }
+            "3" = { type = "private", cidr_block = "192.168.0.32/28", availability_zone_index = "c" }
+            # free CIDRs: 192.168.0.48/28
           }
         },
-        "vpc_a-copy" = {
+        "vpc_acopy" = {
           cidr_block = "10.0.0.0/24"
           subnets = {
-            "1" = { type = "generic", cidr_block = "10.0.0.0/26", availability_zone_index = "a" }
-            "2" = { type = "public",  cidr_block = "10.0.0.64/26", availability_zone_index = "b" }
-            "3" = { type = "private", cidr_block = "10.0.0.128/25", availability_zone_index = "c" }
+            "1" = { type = "generic", cidr_block = "10.0.0.0/27", availability_zone_index = "a" }
+            "2" = { type = "public",  cidr_block = "10.0.0.192/27", availability_zone_index = "b" }
+            "3" = { type = "private", cidr_block = "10.0.0.224/27", availability_zone_index = "c" }
+            # free CIDRs: 10.0.0.32/27, 10.0.0.64/27, 10.0.0.96/27, 10.0.0.128/27, 10.0.0.160/27
           }
         }
       }
--- aws/terraform/start_base-infra-setup.sh
+++ aws/terraform/start_base-infra-setup.sh
@@ -9,5 +9,38 @@ cd `pwd`/base-infra-setup
 terraform init
 terraform apply -auto-approve=true
 
+
+#sleep 1m
+echo "Stopping ec2_natgateway-server with specified tags to save costs."
+# First stop-instances command
+aws ec2 stop-instances \
+--region us-east-1 \
+--profile default \
+--instance-ids \
+    $(aws ec2 describe-instances \
+    --filters \
+        "Name=tag:Name,Values=\
+        _terraform.vpc_b.subnet_generic-az_a.ec2_natgateway-server" \
+        "Name=instance-state-name,Values=running" \
+    --output text --query 'Reservations[*].Instances[*].InstanceId' \
+    --region us-east-1 \
+    --profile default)
+
+# Second stop-instances command
+aws ec2 stop-instances \
+--region eu-west-2 \
+--profile secondary \
+--instance-ids \
+    $(aws ec2 describe-instances \
+    --filters \
+        "Name=tag:Name,Values=\
+        _terraform.vpc_c.subnet_generic-az_a.ec2_natgateway-server,\
+        _terraform.vpc_acopy.subnet_generic-az_a.ec2_natgateway-server" \
+        "Name=instance-state-name,Values=running" \
+    --output text --query 'Reservations[*].Instances[*].InstanceId' \
+    --region eu-west-2 \
+    --profile secondary)
+
+
 cd `pwd`
 echo "[END] installing 'base-infra-setup'..."
