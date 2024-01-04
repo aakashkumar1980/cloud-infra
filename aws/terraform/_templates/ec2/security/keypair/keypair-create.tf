fba4d68cb8547670833b89be4107bf81db8a3265
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-01-04 00:42:41 -0700

    updated

--- aws/terraform/_templates/ec2/security/keypair/keypair-create.tf
+++ aws/terraform/_templates/ec2/security/keypair/keypair-create.tf
@@ -1,5 +1,5 @@
 data "http" "keypair_pubickey" {
-  url = "https://raw.githubusercontent.com/aakashkumar1980/apps-configs/main/security/ssh/id_rsa_ec2.pub"
+  url = "https://raw.githubusercontent.com/aakashkumar1980/apps-configs/main/security/ssh/keys/id_rsa_ec2.pub"
 }
 
 resource "aws_key_pair" "keypair" {
