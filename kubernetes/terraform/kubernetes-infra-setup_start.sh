f2504adf163d3a21070a11d946a623bbfbf19d60
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-01-31 00:04:17 -0700

    update

--- kubernetes/terraform/kubernetes-infra-setup_start.sh
+++ kubernetes/terraform/kubernetes-infra-setup_start.sh
@@ -7,8 +7,8 @@ echo "[START] installing 'kubernetes-infra-setup'..."
 ../../aws/terraform/_ec2_natgateway_server-start.sh
 
 # FINALLY INSTALL KUBERNETES INFRASTRUCTURE
-echo "Installing kubernetes-infra-setup in 3 minutes..."
-sleep 3m
+echo "Installing kubernetes-infra-setup in 4 minutes..."
+sleep 4m
 cd `pwd`/servers
 terraform init -upgrade
 terraform apply -auto-approve=true
--- kubernetes/terraform/servers/control_planes/ec2/main.tf
+++ kubernetes/terraform/servers/control_planes/ec2/main.tf
@@ -3,6 +3,16 @@ module "EC2_PRIMARY" {
 
   subnet_id       = var.vpc_c-subnet_private.id
   security_groups = var.security_group_ids
+  user_data       = <<-EOF
+    #!/bin/bash
+
+    # Install the Amazon SSM Agent
+    sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+
+    # Enable and start the SSM Agent service
+    sudo systemctl enable amazon-ssm-agent
+    sudo systemctl start amazon-ssm-agent
+    EOF  
 
   ami                  = var.ami
   instance_type        = var.instance_type
@@ -17,6 +27,16 @@ module "EC2_SECONDARY" {
 
   subnet_id       = var.vpc_c-subnet_private.id
   security_groups = var.security_group_ids
+  user_data       = <<-EOF
+    #!/bin/bash
+
+    # Install the Amazon SSM Agent
+    sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+
+    # Enable and start the SSM Agent service
+    sudo systemctl enable amazon-ssm-agent
+    sudo systemctl start amazon-ssm-agent
+    EOF  
 
   ami                  = var.ami
   instance_type        = var.instance_type
