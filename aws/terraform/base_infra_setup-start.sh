05cf00c56e27e656e3a907f2813da6b9ae6a094e
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-02-29 20:44:45 -0700

    updated

--- aws/terraform/base_infra_setup-start.sh
+++ aws/terraform/base_infra_setup-start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+BASE_PATH=/home/ubuntu/Desktop/cloud-infra/aws/terraform
 # ####################### #
 # BASE-SETUP INSTALLATION #
 # ####################### #
 echo "[START] installing 'base-infra-setup'..."
-cd `pwd`/base-infra-setup
+cd $BASE_PATH/base-infra-setup
 terraform init -upgrade
 terraform apply -auto-approve=true
 
-../_ec2_natgateway_server-stop.sh
+$BASE_PATH/_ec2_natgateway_server-stop.sh
 echo "[END] installing 'base-infra-setup'..."
--- aws/terraform/base_infra_setup-stop.sh
+++ aws/terraform/base_infra_setup-stop.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+BASE_PATH=/home/ubuntu/Desktop/cloud-infra/aws/terraform
+KUBERNETES_PATH=/home/ubuntu/Desktop/cloud-infra/kubernetes
+# ######################## #
+# OTHER SETUP UNSTALLATION #
+# ######################## #
+echo "stopping 'kubernetes-infra-setup' if not stopped..."
+$KUBERNETES_PATH/kubernetes_infra_setup-stop.sh
+
+
 # ####################### #
 # BASE-SETUP UNSTALLATION #
 # ####################### #
-echo "stopping 'kubernetes-infra-setup' if not stopped..."
-../../kubernetes/kubernetes_infra_setup-stop.sh
-
 echo "[STOP] un-installing 'base-infra-setup'..."
-cd `pwd`/base-infra-setup
+cd $BASE_PATH/base-infra-setup
 terraform destroy -auto-approve=true
 
 echo "[END] un-installing 'base-infra-setup'..."
