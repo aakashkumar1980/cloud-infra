425a96fc19027d71b7ff5ae7fd854722484c223f
aakash.kumar1980 <aakash,kumar1980@yahoo.com>
2025-11-04 17:19:29 -0700

    refactored

--- aws/locals.tf
+++ aws/locals.tf
@@ -3,15 +3,19 @@
 * We then pass that data into region-scoped modules using provider aliases.
 */
 locals {
-  network_raw = jsondecode(file(var.network_config_path))
-
+  // common tags for all resources
   common_tags = {
-    Project   = var.project
-    Env       = var.env
-    Owner     = var.owner
-    ManagedBy = "terraform"
+    project   = var.project
+    env       = var.env
+    owner     = var.owner
+    managed_by = "terraform"
   }
 
+  // load raw network config from JSON file
+  network_raw = jsondecode(file(var.network_config_path))
+  // extract region-specific data
+  region_nvirginia = try(local.network_raw.region_nvirginia.region, {})
   region_nvirginia_vpcs = try(local.network_raw.region_nvirginia.vpc, {})
+  region_london      = try(local.network_raw.region_london.region, {})
   region_london_vpcs    = try(local.network_raw.region_london.vpc, {})
 }
--- aws/main.tf
+++ aws/main.tf
@@ -1,2 +1,13 @@
+/** Build VPCs in us-east-1 (N. Virginia) */
+module "region_nvirginia" {
+  source = "./modules/network/region"
+  providers = {
+    aws = aws.nvirginia
+  }
 
+  region             = local.region_nvirginia
+  vpcs               = local.region_nvirginia_vpcs
+  single_nat_gateway = var.single_nat_gateway
+  extra_tags         = local.common_tags
+}
 
